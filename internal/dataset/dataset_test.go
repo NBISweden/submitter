9311d3a0dfbc0a4202ad1d5c43575c93e7c69811
zeidlitz <zeidlitzerik@gmail.com>
2026-01-21 15:38:41 +0100

    tests: draft for more unit tests

---
+++ internal/dataset/dataset_test.go
@@ -0,0 +1,107 @@
+package dataset
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/NBISweden/sda-bpctl/internal/models"
+)
+
+type mockClient struct {
+	UserFiles []models.FileInfo
+	UserFilesWithPrefix []models.FileInfo
+	Response      *http.Response
+}
+
+func (m *mockClient) GetUsersFiles() ([]models.FileInfo, error) {
+	return m.UserFiles, nil
+}
+
+func (m *mockClient) GetUsersFilesWithPrefix() ([]models.FileInfo, error) {
+	return m.UserFilesWithPrefix, nil
+}
+
+func (m *mockClient) PostFileIngest(data []byte) ([]byte, error) {
+	response, err := json.Marshal(m.Response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (m *mockClient) PostFileAccession(payload []byte) ([]byte, error) {
+	response, err := json.Marshal(m.Response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (m *mockClient) PostDatasetCreate(payload []byte) ([]byte, error) {
+	response, err := json.Marshal(m.Response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func newMockClient(userID string, datasetFolder string) *mockClient {
+	mock := &mockClient{
+		UserFilesWithPrefix: []models.FileInfo{
+			// Data mocked so that we expect 2 files to be returned
+			{InboxPath: fmt.Sprintf("/%s/%s/file1.c4gh", userID, datasetFolder), Status: "verified"},
+			{InboxPath: fmt.Sprintf("/%s/%s/file2.c4gh", userID, datasetFolder), Status: "verified"},
+		},
+		Response: &http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(bytes.NewBufferString("ok"))},
+	}
+	return mock
+}
+
+func TestDataset(t *testing.T) {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	workingDirectory := filepath.Dir(ex)
+	datasetFolder := "DATASET_TEST"
+	datasetID := "aa-Dataset-test"
+	userID := "testuser"
+	expectedNrFiles := 2
+	mock := newMockClient(userID, datasetFolder)
+
+	t.Run("Test Dataset", func(t *testing.T) {
+		datasetCmd.Flag("data-directory").Value.Set(workingDirectory)
+		var files []models.FileInfo
+		files, err := mock.GetUsersFilesWithPrefix()
+		if err != nil {
+			t.Error(err)
+		}
+
+		// Need some way to clean up the file that is created from this? Maybe rework the function a bit ...
+		err = createStableIDsFile(datasetFolder, files)
+		if err != nil {
+			t.Error(err)
+		}
+
+		fileIDsList, err := getFileIDsFromFile(datasetFolder)
+		if err != nil {
+			t.Error(err)
+		}
+
+		nrFiles := len(fileIDsList)
+		if nrFiles != expectedNrFiles {
+			t.Logf("recieved %d/%d paths for accessionIDs", nrFiles, expectedNrFiles)
+			t.Fail()
+		}
+
+		err = createDataset(mock, datasetID, userID, fileIDsList)
+
+	})
+}
